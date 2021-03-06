6c66c720c38998f2247c0635b6e8697810735f22
Damodar Lohani <dlohani48@gmail.com>
2021-03-06 09:33:06 +0545

    model

---
+++ lib/models/channel.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import 'package:flappwritechat/models/message.dart';
+
+class Channel {
+  final String id;
+  final String title;
+  final List<Message> messages;
+  Channel({
+    this.id,
+    this.title,
+    this.messages,
+  });
+
+  Channel copyWith({
+    String id,
+    String title,
+    List<Message> messages,
+  }) {
+    return Channel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      messages: messages ?? this.messages,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'messages': messages?.map((x) => x?.toMap())?.toList(),
+    };
+  }
+
+  factory Channel.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Channel(
+      id: map['id'],
+      title: map['title'],
+      messages:  List<Message>.from((map['messages'] ?? []).map((x) => Message.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Channel.fromJson(String source) => Channel.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Channel(id: $id, title: $title, messages: $messages)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Channel &&
+      o.id == id &&
+      o.title == title &&
+      listEquals(o.messages, messages);
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ title.hashCode ^ messages.hashCode;
+}
---
+++ lib/models/message.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+class Message {
+  final String content;
+  final String id;
+  final String senderId;
+  final String senderName;
+  Message({
+    this.content,
+    this.id,
+    this.senderId,
+    this.senderName,
+  });
+
+  Message copyWith({
+    String content,
+    String id,
+    String senderId,
+    String senderName,
+  }) {
+    return Message(
+      content: content ?? this.content,
+      id: id ?? this.id,
+      senderId: senderId ?? this.senderId,
+      senderName: senderName ?? this.senderName,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'content': content,
+      'id': id,
+      'senderId': senderId,
+      'senderName': senderName,
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Message(
+      content: map['content'],
+      id: map['id'],
+      senderId: map['senderId'],
+      senderName: map['senderName'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Message.fromJson(String source) => Message.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Message(content: $content, id: $id, senderId: $senderId, senderName: $senderName)';
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Message &&
+      o.content == content &&
+      o.id == id &&
+      o.senderId == senderId &&
+      o.senderName == senderName;
+  }
+
+  @override
+  int get hashCode {
+    return content.hashCode ^
+      id.hashCode ^
+      senderId.hashCode ^
+      senderName.hashCode;
+  }
+}
